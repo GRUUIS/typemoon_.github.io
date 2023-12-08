e21a5b1877fada109941a87ac17db64b8220e7d3
GRUUIS <yufanz1114@outlook.com>
2023-12-08 16:46:01 +0800

    1

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,74 +1,86 @@
 /*asked ChatGPT; this code gets elements in that carousel container, then 
 change its style's transform in X-axis*/
 // Initialize variables
-let currentSlide = 0; 
-const slidesContainer = document.querySelector('.carousel-slides'); 
-const totalSlides = document.querySelectorAll('.carousel-slide').length; 
-const transitionDuration = 1500; 
-const autoPlayInterval = 8000; 
-
-// Variable to store initial touch position
+let currentSlide = 0;
+const slidesContainer = document.querySelector('.carousel-slides');
+const totalSlides = document.querySelectorAll('.carousel-slide').length;
+const transitionDuration = 1500;
+const autoPlayInterval = 8000;
 let touchStartX = 0;
+let isTouching = false; // Flag to track touch interaction
 
 // Function to show a specific slide based on its index
 function showSlide(index) {
-    const offset = -index * 100 + '%'; 
-    slidesContainer.style.transition = `transform ${transitionDuration / 1000}s ease-out`; 
-    slidesContainer.style.transform = `translateX(${offset})`; 
+    const offset = -index * 100 + '%';
+    slidesContainer.style.transition = `transform ${transitionDuration / 1000}s ease-out`;
+    slidesContainer.style.transform = `translateX(${offset})`;
 }
 
 // Function to move to the next slide
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides; 
-    showSlide(currentSlide); 
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
 }
 
 // Function to move to the previous slide
 function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides; 
-    showSlide(currentSlide); 
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
 }
 
 // Function for auto-playing slides
 function autoPlay() {
-    nextSlide(); 
+    nextSlide();
 }
 
-// Set up an interval for auto-playing slides；8s
+// Set up an interval for auto-playing slides (every 8 seconds)
 let autoPlayTimer = setInterval(autoPlay, autoPlayInterval);
 
 // Function to restart auto-play after manual slide navigation
 function startAutoPlay() {
-    autoPlayTimer = setInterval(autoPlay, autoPlayInterval);
+    if (!isTouching) {
+        autoPlayTimer = setInterval(autoPlay, autoPlayInterval);
+    }
 }
 
 // Event listeners for previous and next button clicks
 document.getElementById('prevBtn').addEventListener('click', () => {
-    prevSlide(); 
-    clearInterval(autoPlayTimer); 
-    startAutoPlay(); 
+    prevSlide();
+    clearInterval(autoPlayTimer);
+    startAutoPlay();
 });
 
 document.getElementById('nextBtn').addEventListener('click', () => {
-    nextSlide(); 
-    clearInterval(autoPlayTimer); 
-    startAutoPlay(); 
+    nextSlide();
+    clearInterval(autoPlayTimer);
+    startAutoPlay();
 });
 
 // Event listeners for touch events to enable swipe navigation
 slidesContainer.addEventListener('touchstart', (e) => {
     touchStartX = e.touches[0].clientX;
+    isTouching = true;
 });
 
 slidesContainer.addEventListener('touchend', (e) => {
     const touchEndX = e.changedTouches[0].clientX;
     const deltaX = touchEndX - touchStartX;
 
-    // Set a threshold for the swipe distance to prevent accidental swipes
     if (Math.abs(deltaX) > 50) {
-        // If positive, swipe right; if negative, swipe left
         deltaX > 0 ? prevSlide() : nextSlide();
         clearInterval(autoPlayTimer);
         startAutoPlay();
     }
+
+    isTouching = false;
 });
+
+// Optional: Add touch cancel event listener
+slidesContainer.addEventListener('touchcancel', () => {
+    isTouching = false;
+});
+
+// Optional: Add touch move event listener for additional interactions
+// slidesContainer.addEventListener('touchmove', (e) => {
+//     // Handle touch move if needed
+// });
