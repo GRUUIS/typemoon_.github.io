da8103acac8c4bb3c78b5baac9805da990238081
GRUUIS <yufanz1114@outlook.com>
2023-12-09 22:02:29 +0800

    1

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,6 +1,4 @@
-/*asked ChatGPT; 
-And Youtube tutorial credited in readMe.md
-this code gets elements in that carousel container, then 
+/*asked ChatGPT; this code gets elements in that carousel container, then 
 change its style's transform in X-axis*/
 // Initialize variables
 let currentSlide = 0;
@@ -59,121 +57,34 @@ document.getElementById('nextBtn').addEventListener('click', () => {
     startAutoPlay();
 });
 
-// Event listeners for touch events to enable swipe navigation
-slidesContainer.addEventListener('touchstart', handleTouchStart);
-slidesContainer.addEventListener('touchmove', handleTouchMove);
-slidesContainer.addEventListener('touchend', handleTouchEnd);
-
-// Touch start event handler
-function handleTouchStart(e) {
+// Touch events for swipe functionality
+slidesContainer.addEventListener('touchstart', (e) => {
     touchStartX = e.touches[0].clientX;
     touchStartY = e.touches[0].clientY;
     isTouching = true;
-}
+});
 
-// Touch move event handler
-function handleTouchMove(e) {
-    const touchMoveX = e.touches[0].clientX;
-    const touchMoveY = e.touches[0].clientY;
-    const deltaX = touchMoveX - touchStartX;
-    const deltaY = touchMoveY - touchStartY;
-
-    // Calculate the angle of the swipe
-    const angle = Math.atan2(Math.abs(deltaY), Math.abs(deltaX)) * (180 / Math.PI);
-
-    // If the angle is less than 45 degrees, consider it a horizontal swipe
-    if (angle < 45) {
-        const percentageDeltaX = (deltaX / window.innerWidth) * 100;
-
-        slidesContainer.style.transition = 'none';
-        slidesContainer.style.transform = `translateX(-${currentSlide * 100 + percentageDeltaX}%)`;
-
-        e.preventDefault(); // Prevent default touchmove behavior to improve responsiveness
-    }
-}
-
-// Touch end event handler
-function handleTouchEnd(e) {
+slidesContainer.addEventListener('touchmove', (e) => {
     if (!isTouching) return;
 
-    const touchEndX = e.changedTouches[0].clientX;
+    const touchEndX = e.touches[0].clientX;
+    const touchEndY = e.touches[0].clientY;
     const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
 
-    if (Math.abs(deltaX) > 40) {
-        deltaX > 0 ? prevSlide() : nextSlide();
-        clearInterval(autoPlayTimer);
-        startAutoPlay();
-    } else {
-        showSlide(currentSlide); // Reset to the current slide if the movement is less than the threshold
+    // Check if the swipe is horizontal
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Swipe left
+        if (deltaX > 0) {
+            prevSlide();
+        }
+        // Swipe right
+        else {
+            nextSlide();
+        }
     }
 
     isTouching = false;
-}
-
-// Additional code for carousel with mouse drag interaction
-const carousel = document.querySelector(".carousel-slides"),
-    firstImg = carousel.querySelectorAll("img")[0],
-    arrowIcons = document.querySelectorAll(".wrapper i");
-
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-
-const showHideIcons = () => {
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
-}
-
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14;
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60);
-    });
+    clearInterval(autoPlayTimer);
+    startAutoPlay();
 });
-
-const autoSlide = () => {
-    if (carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-
-    positionDiff = Math.abs(positionDiff);
-    let firstImgWidth = firstImg.clientWidth + 14;
-    let valDifference = firstImgWidth - positionDiff;
-
-    if (carousel.scrollLeft > prevScrollLeft) {
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-    }
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-}
-
-const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
-
-const dragging = (e) => {
-    if (!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
-}
-
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-
-    if (!isDragging) return;
-    isDragging = false;
-    autoSlide();
-}
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
-
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
